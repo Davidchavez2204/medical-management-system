7c50bb72c80fbcbdb518ecb8f75857b587ca512b
Davidchavez2204 <adce.2204@gmail.com>
2024-11-07 20:19:29 -0500

    update test

--- Backend/medicalmanagement/src/test/java/cl/ipchile/medicalmanagement/MedicalmanagementApplicationTests.java
+++ Backend/medicalmanagement/src/test/java/cl/ipchile/medicalmanagement/MedicalmanagementApplicationTests.java
@@ -2,12 +2,13 @@ package cl.ipchile.medicalmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Usa el perfil de prueba
 class MedicalmanagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
